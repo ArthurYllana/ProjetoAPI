2d14cd2c8c7ffecc5dda4b34f1d9702950ba6660
ArthurYllana <arthur.yllana@gmail.com>
2025-04-29 12:13:41 -0300

    Aprendendo conceitos básicos de API, maior parte foi criado pelo professor

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\Api\V1\LoginUserRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        
+    }
+}
